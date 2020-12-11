22a00f8fe1c4107a94d06615641aef43a930c909
Muhsin ARSLAN <muhsinarslandev@gmail.com>
2020-12-11 04:22:12 +0300

    Update

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,14 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is FUN!");
 console.log(20 + 10 + 33);
+
+let firstName = "Muhsin";
+console.log(firstName);
+
+let country = "Turkey";
+let continent = "Europe";
+let population = 70000000;
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population);
