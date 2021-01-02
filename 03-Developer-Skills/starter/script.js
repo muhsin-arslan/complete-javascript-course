e0e29f585d612af8f2635a24e6859815752b9f52
Muhsin ARSLAN <developermuhsin@gmail.com>
2021-01-03 02:47:42 +0300

    Update

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,2 +1,40 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+/* 
+// Debug via console.log
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "cels",
+    // FIX BUG
+    value: Number(prompt("Degrees celcius:")),
+  };
+
+  // FIND BUG
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// IDENTIFY BUG
+console.log(measureKelvin()); */
+
+// Coding Challange
+
+const forecast = {
+  temps: [17, 21, 23],
+  temps2: [12, 5, -5, 0, 4],
+  print: function (temps) {
+    const lengthOfTemps = temps.length;
+    let forecastString = "";
+    for (let i = 0; i < lengthOfTemps; i++) {
+      if (i == lengthOfTemps - 1)
+        forecastString += ` ... ${temps[i]}C in ${i + 1} days ... `;
+      else forecastString += ` ... ${temps[i]}C in ${i + 1} days `;
+    }
+    console.log(forecastString);
+  },
+};
+
+forecast.print(forecast.temps2);
